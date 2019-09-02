ee55b56097a4c027ae1fbe783d806b50caa693b2
Markpajr <Shadowstar88@Hotmail.com>
2019-09-02 02:23:37 -0400

    Added zip dictionary

--- Practice Projects/chapter 7 Regex.py
+++ Practice Projects/chapter 7 Regex.py
@@ -2,14 +2,15 @@ import string
 import random
 import re
 import pyperclip
+import pprint
 # DONE: Write Function to detect password strength
 # DONE: Use Reg Ex to determine if password is strong
 # DONE: Get password from clipboard
 # DONE: Strong password : 8+ Char Length, Upper & lower, 1 digit
 # TODO: Pull list of passwords from excel and test strength
 # TODO: Output strength list back to excel
-# TODO: Improve strength to 16 characters, one special character
-# TODO: Generate random list of passwords
+# DONE: Improve strength to 16 characters, one special character
+# DONE: Generate random list of passwords
 
 upperRegex = re.compile(r'[A-Z]+')
 lowerRegex = re.compile(r'[a-z]+')
@@ -29,16 +30,19 @@ if passTest is True:
 else:
     print("Weak Password")
 
-# Creates a list of 15 randomly generated passwords.
-# 10-36 Characters including punctuation, Upper/Lower letters, and Digits
+# Creates a list of 20 randomly generated passwords.
+# 5-36 Characters including punctuation, Upper/Lower letters, and Digits
 randPassList = []
-for i in range(0, 15):
-    randPass = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(random.randint(8, 36))])
+for i in range(0, 20):
+    randPass = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(random.randint(5, 36))])
     randPassList.append(randPass)
 
+strengthList = []
 for password in range(len(randPassList)):
     randTest = passwordstrength(randPassList[password])
     if randTest is True:
-        print("Strong Password")
+        strengthList.append("Strong")
     else:
-        print("Weak Password")
+        strengthList.append("Weak")
+RandPassDict = dict(zip(randPassList, strengthList))
+pprint.pprint(RandPassDict)
